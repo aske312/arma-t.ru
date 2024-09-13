121b7fda7b72d9ece1aae7db81faf458538fc52a
aske.312 <aske312@yandex.ru>
2024-09-13 15:05:56 +0300

    fix by notebook

--- public_html/error.php
+++ public_html/error.php
@@ -1,4 +1,4 @@
-<!-- <?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');?> -->
+<!-- < ?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');?> -->
 <?php Asset::getInstance()->addCss("/local/css/error.css");  //css ?>
 
-<!-- <?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?> -->
+<!-- < ?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?> -->
