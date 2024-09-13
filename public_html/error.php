d56ed460e99f83b169ec7051b31b4b784019ab77
aske.312 <aske312@yandex.ru>
2024-09-13 15:10:17 +0300

    fix by notebook

--- public_html/error.php
+++ public_html/error.php
@@ -1,4 +1,93 @@
-<!-- < ?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');?> -->
+
 <?php Asset::getInstance()->addCss("/local/css/error.css");  //css ?>
 
-<!-- < ?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?> -->
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="ru">
+	<head>
+		<title>arma-t.ru | error page</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="{% static 'css\def_base.css' %}" media="screen">
+		<link rel="stylesheet" href="{% static 'css\default.css' %}" media="screen">
+		<script class="u-script" type="text/javascript" src="{% static 'js\jquery.js' %}" defer=""></script>
+		<script type="application/ld+json">{
+			"@context": "http://schema.org",
+			"@type": "Organization",
+			"name": "",
+			"sameAs": [
+					"https://t.me/aske312",
+					"https://vk.com/aske312",
+					"https://github.com/aske312/arma-t.ru"]}</script>
+	</head>
+	<body class="u-body u-stick-footer u-xl-mode">
+		<header class="u-clearfix u-header u-sticky u-sticky-b03a" id="sec-88d6">
+			<div class="u-clearfix u-sheet u-sheet-1"></div>
+		</header>
+		<section class="u-clearfix u-container-align-center u-section-1" id="sec-3aa0">
+			<div class="u-clearfix u-sheet u-sheet-1">
+				<img class="u-image u-image-contain u-image-default u-image-1" src="{% static 'img\default.png' %}" data-image-width="850" data-image-height="400">
+			</div>
+		</section>
+		<footer class="u-align-center u-black u-clearfix u-footer u-footer" id="sec-9f13">
+			<div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
+				<p class="u-align-center u-small-text u-text u-text-variant u-text-1">
+					The site is still under development, you can follow us in the networks.
+				</p>
+				<div class="u-social-icons u-spacing-10 u-social-icons-1">
+					<a class="u-social-url" target="_blank" data-type="Telegram" title="Telegram" href="https://t.me/aske312">
+						<span class="u-icon u-social-icon u-social-telegram u-text-black u-icon-1">
+							<svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style="">
+								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-9596"></use>
+							</svg>
+							<svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-9596">
+								<circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle>
+								<path fill="#FFFFFF" d="M18.4,53.2l64.7-24.9c3-1.1,5.6,0.7,
+								4.7,5.3l0,0l-11,51.8c-0.8,3.7-3,4.6-6.1,2.8L53.9,75.8l-8.1,
+								7.8 c-0.9,0.9-1.7,1.6-3.4,1.6l1.2-17l31.1-28c1.4-1.2-0.3-1.9-2.1-0.7L34.2,
+								63.7l-16.6-5.2C14,57.4,14,54.9,18.4,53.2L18.4,53.2z"></path>
+							</svg>
+						</span>
+					</a>
+					<a class="u-social-url" target="_blank" data-type="VK" title="VK" href="https://vk.com/aske312">
+						<span class="u-icon u-social-icon u-social-vk u-text-black u-icon-2">
+							<svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style="">
+								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-ff5e"></use>
+							</svg>
+							<svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-ff5e">
+								<circle fill="currentColor" class="st0" cx="56.1" cy="56.1" r="55"></circle>
+								<path fill="#FFFFFF" d="M88.2,80.7l-9.8,0.1c0,0-2.1,0.4-4.9-1.5c-3.7-2.5-7.2-9.1-9.9-8.2C60.9,
+								72,61,77.9,61,77.9s0,1.3-0.6,1.9 c-0.7,0.7-2,0.9-2,0.9H54c0,0-9.7,
+								0.6-18.3-8.3c-9.3-9.7-17.6-29-17.6-29s-0.5-1.3,0-1.9c0.6-0.7,2.2-0.7,
+								2.2-0.7l10.5-0.1 c0,0,1,0.2,1.7,0.7c0.6,0.4,0.9,1.2,0.9,1.2s1.7,4.3,4,
+								8.2c4.4,7.6,6.4,9.3,7.9,8.4c2.2-1.2,1.5-10.7,1.5-10.7s0-3.5-1.1-5
+								c-0.9-1.2-2.5-1.5-3.3-1.6c-0.6-0.1,0.4-1.5,1.6-2.1c1.9-0.9,5.2-1,9.2-0.9c3.1,
+								0,4,0.2,5.2,0.5c3.6,0.9,2.4,4.3,2.4,12.4 c0,2.6-0.5,6.3,1.4,7.5c0.8,
+								0.5,2.8,0.1,7.7-8.3c2.3-4,4.1-8.6,4.1-8.6s0.4-0.8,1-1.2s1.4-0.3,
+								1.4-0.3l11.1-0.1c0,0,3.3-0.4,3.9,1.1 c0.6,1.6-1.2,5.3-5.8,11.3c-7.4,
+								9.9-8.3,9-2.1,14.7c5.9,5.5,7.1,8.1,7.3,8.5C93.4,80.4,88.2,80.7,88.2,80.7z"></path>
+							</svg>
+						</span>
+					</a>
+					<a class="u-social-url" target="_blank" data-type="Github" title="Github" href="https://github.com/aske312/achif.online">
+						<span class="u-icon u-social-github u-social-icon u-text-black u-icon-3">
+							<svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style="">
+								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-9af8"></use>
+							</svg>
+							<svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-9af8">
+								<circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle>
+								<path fill="#FFFFFF" d="M88,51.3c0-5.5-1.9-10.2-5.3-13.7c0.6-1.3,
+								2.3-6.5-0.5-13.5c0,0-4.2-1.4-14,5.3c-4.1-1.1-8.4-1.7-12.7-1.8 c-4.3,
+								0-8.7,0.6-12.7,1.8c-9.7-6.6-14-5.3-14-5.3c-2.8,7-1,12.2-0.5,13.5C25,
+								41.2,23,45.7,23,51.3c0,19.6,11.9,23.9,23.3,25.2 c-1.5,1.3-2.8,
+								3.5-3.2,6.8c-3,1.3-10.2,3.6-14.9-4.3c0,0-2.7-4.9-7.8-5.3c0,
+								0-5-0.1-0.4,3.1c0,0,3.3,1.6,5.6,7.5c0,0,3,9.1,17.2,6 c0,4.3,0.1,
+								8.3,0.1,9.5h25.2c0-1.7,0.1-7.2,0.1-14c0-4.7-1.7-7.9-3.4-9.4C76,
+								75.2,88,70.9,88,51.3z"></path>
+							</svg>
+						</span>
+					</a>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
