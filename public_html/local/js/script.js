aaafac6dcdc6b0a01af199c79aa2914634ee1b05
Aske312 <Aske312@yandex.ru>
2024-09-25 04:33:22 +0300

    fix header

--- public_html/local/js/script.js
+++ public_html/local/js/script.js
@@ -4,6 +4,19 @@ function toggleMenu() {
     nav.classList.toggle('menu-open');
 }
 
+window.onscroll = function() {stickyHeader()};
+
+var header = document.getElementById("siteHeader");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
+}
+
 //***** index.php *****//
 let slideIndex = 0; // Изначальный индекс слайда
 let slides = document.getElementsByClassName('slide'); // Получаем все слайды
