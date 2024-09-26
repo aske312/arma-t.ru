6f367966d1b602112846766614a08ed91220a3b3
aske.312 <aske312@yandex.ru>
2024-09-26 13:25:42 +0300

    fix by notebook

--- public_html/catalog/index.php
+++ public_html/catalog/index.php
@@ -192,7 +192,7 @@ $arResult['NAV_STRING'] = $res->GetPageNavStringEx($navComponentObject, "", ".de
                                 <p><div class="catalog-item-price"><?= $arProps['EL_PRICE']['VALUE']; ?> руб.</div></p>
                             </div>
                             <div class="catalog-item-controls">
-                                <button class="catalog-item-add-to-cart" onclick="addToCart(<?= $arFields['ID']; ?>, '<?= $arProps['EL_ARTICUL']['VALUE']; ?>', <?= $arProps['EL_PRICE']['VALUE']; ?>)">В корзину</button>
+                                <button class="catalog-item-add-to-cart" onclick="addToCart(<?= $arFields['ID'], $arProps['EL_ARTICUL']['VALUE'], $arProps['EL_PRICE']['VALUE']; ?>)">В корзину</button>
                             </div>
                         </div>
 
