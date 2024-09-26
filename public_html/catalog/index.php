bf5ecc82e5c8e8af06626b9b76b03fecceb1c198
Aske312 <Aske312@yandex.ru>
2024-09-26 03:13:38 +0300

    fix catalog

--- public_html/catalog/index.php
+++ public_html/catalog/index.php
@@ -242,21 +242,4 @@ $arResult['NAV_STRING'] = $res->GetPageNavStringEx($navComponentObject, "", ".de
             </div>
         </div>
 
-        <script>
-            // Функция для добавления выбранных товаров в корзину
-            document.querySelector('.catalog-add-all').addEventListener('click', function() {
-                const checkboxes = document.querySelectorAll('.catalog-item-checkbox:checked');
-                checkboxes.forEach(function(checkbox) {
-                    const itemId = checkbox.id.replace('item-', '');
-                    addToCart(itemId);
-                });
-            });
-
-            // Функция для выделения всех товаров
-            function toggleSelectAll(source) {
-                const checkboxes = document.querySelectorAll('.catalog-item-checkbox');
-                checkboxes.forEach(checkbox => checkbox.checked = source.checked);
-            }
-        </script>
-
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php"); ?>
