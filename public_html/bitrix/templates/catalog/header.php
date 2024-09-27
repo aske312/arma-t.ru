ccf2acc575238bc0b6243b96025c3e383fb696c5
aske.312 <aske312@yandex.ru>
2024-09-27 17:43:54 +0300

    fix by notebook

--- public_html/bitrix/templates/catalog/header.php
+++ public_html/bitrix/templates/catalog/header.php
@@ -1,4 +1,6 @@
-<?php if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
+<?php if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+Asset::getInstance()->addJs("/local/js/script.js");
+?>
 
 <!DOCTYPE html>
 <html lang="ru">
