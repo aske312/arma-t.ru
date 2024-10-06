1a0e8fc6d63beff8d80acb66bca309eec68589ad
Aske312 <Aske312@yandex.ru>
2024-10-06 23:27:46 +0300

    fix

--- public_html/bitrix/templates/home/header.php
+++ public_html/bitrix/templates/home/header.php
@@ -1,4 +1,4 @@
-<?php if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
+<?php if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();?>
 
 <!DOCTYPE html>
 <html lang="ru">
@@ -8,36 +8,87 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Описание страницы для SEO">
     <meta name="keywords" content="Ключевые слова для SEO">
-    <?php $APPLICATION->ShowHead(); ?>
-    <title><?php $APPLICATION->ShowTitle(); ?></title>
+    <?$APPLICATION->ShowHead();?>
+    <title><?$APPLICATION->ShowTitle();?></title>
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
 </head>
-<body>
-    <div id="panel"> <?php $APPLICATION->ShowPanel(); ?> </div>
-    <header id="siteHeader">
-        <div class="header-content">
-            <div class="logo">
-                <a href="/"><img src="/local/img/logo/resource_1.png" alt="My Logo"></a>
-            </div>
-            <div class="nav-search">
-                <button class="menu-toggle" onclick="toggleMenu()">&#9776;</button>
-                <nav id="mainNav">
-                    <a href="/#Company">О компании</a>
-                    <a href="/catalog/">Каталог</a>
-                    <a href="/#Contact">Контакты</a>
-                    <a href="/#Delivery">Доставка</a>
-                    <a href="/#Cash">Оплата</a>
-                </nav>
-            </div>
-            <div class="contact-info">
-                <p><a href="tel:+70000000000" class="phone-link">+7 (000) 000-00-00</a></p>
-                <button onclick="window.location.href='#Cash'">Оставить заявку</button>
+    <body>
+        <div id="panel"> <?$APPLICATION->ShowPanel();?> </div>
+        <header id="siteHeader">
+            <div class="header-content">
+                <div class="logo">
+                    <a href="/"><img src="/local/img/logo/resource_1.png" alt="My Logo"></a>
+                </div>
+                <div class="nav-search">
+                    <button class="menu-toggle" onclick="toggleMenu()">&#9776;</button>
+                    <nav id="mainNav">
+                        <a href="/#Company">О компании</a>
+                        <a href="/catalog/">Каталог</a>
+                        <a href="/#Contact">Контакты</a>
+                        <a href="/#Delivery">Доставка</a>
+                        <a href="/#Cash">Оплата</a>
+                    </nav>
+                    <!--
+                        <?php $APPLICATION->IncludeComponent(
+                            "search",
+                            "",
+                            Array(
+                                "ACTION_VARIABLE" => "action",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "Y",
+                                "BASKET_URL" => "/personal/basket.php",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_TYPE" => "A",
+                                "CHECK_DATES" => "N",
+                                "DETAIL_URL" => "",
+                                "DISPLAY_BOTTOM_PAGER" => "Y",
+                                "DISPLAY_COMPARE" => "N",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "ELEMENT_SORT_FIELD" => "sort",
+                                "ELEMENT_SORT_FIELD2" => "id",
+                                "ELEMENT_SORT_ORDER" => "asc",
+                                "ELEMENT_SORT_ORDER2" => "desc",
+                                "IBLOCK_ID" => "",
+                                "IBLOCK_TYPE" => "catalog",
+                                "LINE_ELEMENT_COUNT" => "3",
+                                "NO_WORD_LOGIC" => "N",
+                                "OFFERS_LIMIT" => "5",
+                                "PAGER_DESC_NUMBERING" => "N",
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_SHOW_ALWAYS" => "N",
+                                "PAGER_TEMPLATE" => ".default",
+                                "PAGER_TITLE" => "Товары",
+                                "PAGE_ELEMENT_COUNT" => "30",
+                                "PRICE_CODE" => array(),
+                                "PRICE_VAT_INCLUDE" => "Y",
+                                "PRODUCT_ID_VARIABLE" => "id",
+                                "PRODUCT_PROPERTIES" => array(),
+                                "PRODUCT_PROPS_VARIABLE" => "prop",
+                                "PRODUCT_QUANTITY_VARIABLE" => "quantity",
+                                "PROPERTY_CODE" => array("",""),
+                                "RESTART" => "N",
+                                "SECTION_ID_VARIABLE" => "SECTION_ID",
+                                "SECTION_URL" => "",
+                                "SHOW_PRICE_COUNT" => "1",
+                                "USE_LANGUAGE_GUESS" => "Y",
+                                "USE_PRICE_COUNT" => "N",
+                                "USE_PRODUCT_QUANTITY" => "N",
+                                "USE_SEARCH_RESULT_ORDER" => "N",
+                                "USE_TITLE_RANK" => "N"
+                            )
+                        );?> -->
+                </div>
+                <div class="contact-info">
+                    <p><a href="tel:+70000000000" class="phone-link">+7 (000) 000-00-00</a></p>
+                    <button onclick="window.location.href='#Cash'">Оставить заявку</button>
 
-                <!-- Корзина -->
-                <?php if ($cartItemCount > 0): // Проверка, есть ли товары в корзине ?>
-                <div class="basket-container">
-                    <button class="basket-toggle" onclick="toggleBasket()">Корзина (<?php echo $cartItemCount; ?>)</button>
-                    <div class="basket-dropdown" id="basketDropdown">
+                    <!-- Корзина -->
+                    <div class="basket">
+                        <h3>Корзина</h3>
                         <table>
                             <thead>
                             <tr>
@@ -49,15 +100,15 @@
                             </tr>
                             </thead>
                             <tbody id="basket-items">
-                                <!-- Товары будут добавлены динамически -->
+
+                            <!-- Товары будут добавлены динамически -->
                             </tbody>
                         </table>
                         <p>Итоговая стоимость: <span id="total-price">0</span> руб.</p>
                         <button id="checkout">Оформить заказ</button>
                         <button id="clear-cart">Очистить корзину</button>
                     </div>
+
                 </div>
-                <?php endif; ?>
             </div>
-        </div>
-    </header>
+        </header>
