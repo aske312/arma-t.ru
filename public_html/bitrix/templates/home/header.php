9e3e0f426e18632b00712ca59465adfb17a30f81
Aske312 <Aske312@yandex.ru>
2024-10-08 01:51:53 +0300

    fix

--- public_html/bitrix/templates/home/header.php
+++ public_html/bitrix/templates/home/header.php
@@ -4,7 +4,14 @@
 use Bitrix\Main\Loader;
 use Bitrix\Main\Page\Asset;
 
+Loader::includeModule('iblock');
+
+// Подключение стилей и скриптов
+Asset::getInstance()->addCss("/local/css/catalog.css");
+Asset::getInstance()->addCss("/local/css/header.css");
+Asset::getInstance()->addCss("/local/css/footer.css");
 Asset::getInstance()->addJs("/local/js/script.js");
+
 ?>
 
 <!DOCTYPE html>
--- public_html/catalog/index.php
+++ public_html/catalog/index.php
@@ -1,16 +1,16 @@
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 // Подключение модулей и стилей
-use Bitrix\Main\Loader;
-use Bitrix\Main\Page\Asset;
+//use Bitrix\Main\Loader;
+//use Bitrix\Main\Page\Asset;
 
-Loader::includeModule('iblock');
+//Loader::includeModule('iblock');
 
 // Подключение стилей и скриптов
-Asset::getInstance()->addCss("/local/css/catalog.css");
-Asset::getInstance()->addCss("/local/css/header.css");
-Asset::getInstance()->addCss("/local/css/footer.css");
-Asset::getInstance()->addJs("/local/js/script.js");
+//Asset::getInstance()->addCss("/local/css/catalog.css");
+//Asset::getInstance()->addCss("/local/css/header.css");
+//Asset::getInstance()->addCss("/local/css/footer.css");
+//Asset::getInstance()->addJs("/local/js/script.js");
 
 // Получение списка секций для бокового меню
 $sectionsFilter = [
