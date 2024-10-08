0bc40fe677b4d4470148ffb61aaddc2f54153050
Aske312 <Aske312@yandex.ru>
2024-10-08 04:19:53 +0300

    fix

--- public_html/catalog/catalog.php
+++ public_html/catalog/catalog.php
@@ -4,6 +4,10 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 // Берем значение секции
 $sectionId = intval($_GET['SECTION_ID']);
 
+use Bitrix\Main\Page\Asset;
+
+Asset::getInstance()->addCss("/local/css/catalog.css");
+
 // Получение текущей секции
 $sectionFilter = [
     'IBLOCK_ID' => $arParams['IBLOCK_ID'],
@@ -295,5 +299,67 @@ echo '</pre>'; // Закрываем тег <pre>
                 <?= $arResult['NAV_STRING']; ?>
             </div>
         </div>
+
+        <script>
+            // Функция для выбора всех товаров
+            function toggleSelectAll(checkbox) {
+                const checkboxes = document.querySelectorAll('.catalog-item-checkbox');
+                checkboxes.forEach(item => {
+                    item.checked = checkbox.checked;
+                });
+            }
+
+            // Функция перехода на каталог по секциям
+            function redirectToSection(sectionId) {
+                window.location.href = '/catalog/catalog.php?SECTION_ID=' + sectionId;
+            }
+
+            //*** КОРЗИНА ***//
+            document.querySelectorAll('.catalog-item-add-to-cart').forEach(function (button) {
+                button.addEventListener('click', function () {
+                    var productId = this.getAttribute('data-id');
+                    addToCart(productId);
+                });
+            });
+
+            function addToCart(productId) {
+                // Получаем текущие товары из куки
+                var cart = JSON.parse(getCookie('cart') || '[]');
+
+                // Проверяем, есть ли товар уже в корзине
+                if (!cart.includes(productId)) {
+                    cart.push(productId); // Добавляем ID в корзину
+                    setCookie('cart', JSON.stringify(cart), 1); // Сохраняем куки на 7 дней
+                    updateCartCounter();
+                }
+            }
+
+            function updateCartCounter() {
+                var cart = JSON.parse(getCookie('cart') || '[]');
+                var counter = cart.length;
+                document.getElementById('cart-counter').innerText = counter;
+            }
+
+            // Функция для получения куки
+            function getCookie(name) {
+                var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+                return match ? match[2] : null;
+            }
+
+            // Функция для установки куки
+            function setCookie(name, value, days) {
+                var expires = "";
+                if (days) {
+                    var date = new Date();
+                    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                    expires = "; expires=" + date.toUTCString();
+                }
+                document.cookie = name + "=" + (value || "") + expires + "; path=/";
+            }
+
+            // Обновляем счетчик сразу при загрузке страницы
+            updateCartCounter();
+        </script>
+
 
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php"); ?>
--- public_html/index.php
+++ public_html/index.php
@@ -8,9 +8,9 @@ use Bitrix\Iblock;
 
 Loader::includeModule('iblock');
 
-Asset::getInstance()->addCss("/local/css/header.css");  //css
+//Asset::getInstance()->addCss("/local/css/header.css");  //css
 Asset::getInstance()->addCss("/local/css/home.css"); //css
-Asset::getInstance()->addCss("/local/css/footer.css");  //css
+//Asset::getInstance()->addCss("/local/css/footer.css");  //css
 
 $sectionsFilter = [
     'IBLOCK_ID' => $arParams['IBLOCK_ID'],
