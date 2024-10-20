b809f6b79db238580174cdbb2c902c7674f3dcc2
Aske312 <Aske312@yandex.ru>
2024-10-20 21:46:41 +0300

    fix

--- public_html/delivery/index.php
+++ public_html/delivery/index.php
@@ -11,25 +11,22 @@ $IBLOCK_ID = 1; // Замените **1** на ID вашего инфоблок�
 // Получаем данные из инфоблока
 $arSelect = Array("ID", "NAME", "PROPERTY_*");
 $arFilter = Array("IBLOCK_ID" => $IBLOCK_ID, "ACTIVE" => "Y");
-$res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
-
-// Выводим информацию на страницу
-while ($ob = $res->GetNextElement()) {
-    $arFields = $ob->GetFields();
-    $arProps = $ob->GetProperties();
-
-    echo "<h2>{$arFields['NAME']}</h2>";
-    echo "<p>{$arProps['TEXT']['VALUE']}</p>"; // Замените 'TEXT' на название свойства с текстом
-}?>
+$res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect); ?>
 
 <div class="section-title">
     <h2><?php echo htmlspecialchars($productName); ?></h2> <!-- Название раздела -->
     <p>Подробное описание</p>
 </div>
 
+<?php
+    // Выводим информацию на страницу
+    while ($ob = $res->GetNextElement()) {
+        $arFields = $ob->GetFields();
+        $arProps = $ob->GetProperties();
 
+        echo "<h2>{$arFields['NAME']}</h2>";
+        echo "<p>{$arProps['TEXT']['VALUE']}</p>"; // Замените 'TEXT' на название свойства с текстом
+}?>
 
 <button class="back-button" onclick="history.back()">Назад</button> <!-- Кнопка Назад -->
-
 <?php require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php'); ?>
-
