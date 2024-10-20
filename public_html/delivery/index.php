e327cbdfd7daa4fd697b5e69e5ca42681a28eefe
Aske312 <Aske312@yandex.ru>
2024-10-20 22:10:23 +0300

    fix

--- public_html/delivery/index.php
+++ public_html/delivery/index.php
@@ -9,11 +9,12 @@ Asset::getInstance()->addCss("/local/css/delivery.css"); //css
 $IBLOCK_ID = 1; ?>
 
 <div class="section-title">
-    <h2><?php echo htmlspecialchars($productName); ?></h2> <!-- Название раздела -->
-    <p>Подробное описание</p>
+    <h2> Доставка </h2> <!-- Название раздела -->
+    <p> Подробная информация по доставке </p>
 </div>
 
-<button class="back-button" onclick="history.back()">Назад</button> <!-- Кнопка Назад -->
-
+<div class="product-delivery">
+    <button class="back-button" onclick="history.back()">Назад</button> <!-- Кнопка Назад -->
+</div>
 
 <?php require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php'); ?>
--- public_html/payment/index.php
+++ public_html/payment/index.php
@@ -9,11 +9,12 @@ Asset::getInstance()->addCss("/local/css/delivery.css"); //css
 $IBLOCK_ID = 1; ?>
 
 <div class="section-title">
-    <h2><?php echo htmlspecialchars($productName); ?></h2> <!-- Название раздела -->
-    <p>Подробное описание</p>
+    <h2> Оплата </h2> <!-- Название раздела -->
+    <p> Подробная информация по оплате </p>
 </div>
 
-<button class="back-button" onclick="history.back()">Назад</button> <!-- Кнопка Назад -->
-
+<div class="product-payments">
+    <button class="back-button" onclick="history.back()">Назад</button> <!-- Кнопка Назад -->
+</div>
 
 <?php require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php'); ?>
