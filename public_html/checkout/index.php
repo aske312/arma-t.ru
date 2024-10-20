2579d882f6c633b22988dc0a877bc4b5bd7a728a
Aske312 <Aske312@yandex.ru>
2024-10-20 22:18:48 +0300

    fix

--- public_html/checkout/index.php
+++ public_html/checkout/index.php
@@ -5,13 +5,12 @@ Asset::getInstance()->addCss("/local/css/checkout.css"); //css
 
 ?>
 <div class="section-title">
-    <h2><?php echo htmlspecialchars($productName); ?></h2> <!-- Название раздела -->
-    <p>Подробное описание</p>
+    <h2> Оформление заказа </h2> <!-- Название раздела -->
+    <p> Товары в корзине </p>
 </div>
 
-<h1>Оформление заказа</h1>
 <div id="cart-items">
-    <h2>Товары в корзине</h2>
+<div class="product-checkout">
     <?php
     // Получение данных из куки
     $cartItems = json_decode($_COOKIE['cartItems']);
@@ -29,30 +28,26 @@ Asset::getInstance()->addCss("/local/css/checkout.css"); //css
         echo "<p>Ваша корзина пуста.</p>";
     }
     ?>
+
+    <button class="back-button" onclick="history.back()">Назад</button> <!-- Кнопка Назад -->
 </div>
 
 <form class="order-form" id="order-form">
     <h2>Ваши данные</h2>
     <label for="name">Имя:</label>
     <input type="text" id="name" name="name" required>
-
     <label for="email">Email:</label>
     <input type="email" id="email" name="email" required>
-
     <label for="address">Адрес:</label>
     <textarea id="address" name="address" required></textarea>
-
     <button type="submit">Оформить заказ</button>
 </form>
 
 <script>
-    // Обработка отправки формы
     const orderForm = document.getElementById('order-form');
     orderForm.addEventListener('submit', (event) => {
         event.preventDefault();
-        // Здесь вы должны добавить код для отправки данных формы на сервер
-        // Например, используя AJAX или FormData
-        // После успешной отправки вы можете очистить корзину, перенаправить пользователя на страницу подтверждения заказа и т.д.
+
     });
 </script>
 
