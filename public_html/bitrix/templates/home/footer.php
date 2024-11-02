678b44f8ee51a35e591b0f1b84dad0146f40125f
Aske312 <Aske312@yandex.ru>
2024-11-02 18:55:50 +0300

    fix

--- public_html/bitrix/templates/home/footer.php
+++ public_html/bitrix/templates/home/footer.php
@@ -14,6 +14,10 @@
             <div class="map">
                  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A91f7b8e166afeeae94308f6df98ec9af515a64477bc6ed62f27b0ed0b78e66f8&amp;source=constructor" width="475" height="475" frameborder="0"></iframe>
             </div>
+            <h1>Наши реквизиты:</h1>
+            <p>ИНН: 00000000000000</p>
+            <p>ОГРН: 00000000000000</p>
+            <p>КПП: 00000000000000</p>
         </div>
     </div>
     <div class="section footer-title">
