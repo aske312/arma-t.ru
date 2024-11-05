114f01a1415e901ffa69abd943848c8b8eb9f959
Aske312 <Aske312@yandex.ru>
2024-11-05 17:09:53 +0300

    fix

--- public_html/bitrix/templates/home/footer.php
+++ public_html/bitrix/templates/home/footer.php
@@ -16,7 +16,7 @@
                 </div>
             </div>
             <div class="map">
-                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A91f7b8e166afeeae94308f6df98ec9af515a64477bc6ed62f27b0ed0b78e66f8&source=constructor&point=55.681717,37.269466" width="475" height="475" frameborder="0"></iframe>
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A91f7b8e166afeeae94308f6df98ec9af515a64477bc6ed62f27b0ed0b78e66f8&source=constructor&point=37.269466, 55.681717" width="475" height="475" frameborder="0"></iframe>
             </div>
         </div>
     </div>
