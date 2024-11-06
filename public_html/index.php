1da7de26f5eb8d7758bff9899745324b93f1fee4
Aske312 <Aske312@yandex.ru>
2024-11-06 09:53:46 +0300

    fix

--- public_html/index.php
+++ public_html/index.php
@@ -75,58 +75,6 @@ $arResult['SECTIONS'] = [];
 while ($section = $sections->Fetch()) {
     $arResult['SECTIONS'][] = $section;
 }
-
-
-
-use Google\ApiCore\ApiException;
-use Google\Cloud\RecaptchaEnterprise\V1\Client\RecaptchaEnterpriseServiceClient;
-use Google\Cloud\RecaptchaEnterprise\V1\CreateKeyRequest;
-use Google\Cloud\RecaptchaEnterprise\V1\Key;
-use Google\Cloud\RecaptchaEnterprise\V1\WebKeySettings;
-use Google\Cloud\RecaptchaEnterprise\V1\WebKeySettings\IntegrationType;
-
-/**
- * Create a site key for reCAPTCHA
- *
- * @param string $projectId Your Google Cloud project ID
- * @param string $keyName The name of the key you wish to create
- */
-function create_key(string $projectId, string $keyName): void
-{
-    $client = new RecaptchaEnterpriseServiceClient();
-    $formattedProject = $client->projectName($projectId);
-
-    // Create the settings for the key.
-    // In order to create other keys we'll use AndroidKeySettings or IOSKeySettings
-    $settings = new WebKeySettings();
-
-    // Allow the key to work for all domains(Not recommended)
-    $settings->setAllowAllDomains(true);
-    // ...or explicitly set the allowed domains for the key as an array of strings
-    // $settings->setAllowedDomains(['']);
-
-    // Specify the type of the key
-    // - score based key -> IntegrationType::SCORE
-    // - checkbox based key -> IntegrationType::CHECKBOX
-    // Read https://cloud.google.com/recaptcha-enterprise/docs/choose-key-type
-    $settings->setIntegrationType(IntegrationType::CHECKBOX);
-
-    $key = new Key();
-    $key->setDisplayName($keyName);
-    $key->setWebSettings($settings);
-
-    try {
-        $createKeyRequest = (new CreateKeyRequest())
-            ->setParent($formattedProject)
-            ->setKey($key);
-        $createdKey = $client->createKey($createKeyRequest);
-        printf('The key: %s is created.' . PHP_EOL, $createdKey->getName());
-    } catch (ApiException $e) {
-        print('createKey() call failed with the following error: ');
-        print($e);
-    }
-}
-
 ?>
 
 <div class="section section1">
@@ -317,7 +265,7 @@ function create_key(string $projectId, string $keyName): void
             <input type="text" id="subject" name="subject" placeholder="Название Вашей компании" required>
             <textarea id="message" name="message" rows="5" placeholder="Комментарий"></textarea>
 
-            <!-- Добавляем reCAPTCHA -->
+            <!-- Добавляем капчу сюда -->
             <div class="g-recaptcha" data-sitekey="6LcQqHYqAAAAAEs5PRrpZzwzKvKexhGs9BQaHfUH"></div>
 
             <div class="form-actions">
