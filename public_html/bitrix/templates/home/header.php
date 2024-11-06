6f760b86d4b72e1cab5b1fce5f82505104b7ad49
Aske312 <Aske312@yandex.ru>
2024-11-06 15:33:07 +0300

    fix

--- public_html/bitrix/templates/home/header.php
+++ public_html/bitrix/templates/home/header.php
@@ -103,9 +103,7 @@ if ($element = $res->Fetch()) {
                     <button id="cart-button" class="cart-btn">
                         <div class="cart-icon-wrapper">
                             <img src="/resources/img/block/checkout.png" alt="Корзина" class="cart-icon-img">
-                            <?php if ($cartItemCount > 0): ?>
-                                <span id="cart-count" class="cart-count"><?= $cartItemCount ?></span>
-                            <?php endif; ?>
+                            <span id="cart-count" class="cart-count"><?= $cartItemCount ?></span>
                         </div>
                     </button>
                 </div>
@@ -228,6 +226,11 @@ if ($element = $res->Fetch()) {
                 console.error('Ошибка при получении изображения раздела:', error);
                 return '/resources/img/production/0.png';
             }
+        }
+
+        const cartCount = <?= $cartItemCount ?>;
+        if (cartCount === 0) {
+        document.getElementById('cart-count').style.display = 'none';
         }
 
         function updateQuantity(productId, delta) {
