3208c4b7cf20ddf243a589e71e4b4cf39c2062be
Aske312 <Aske312@yandex.ru>
2024-11-06 16:27:11 +0300

    fix

--- public_html/catalog/detail.php
+++ public_html/catalog/detail.php
@@ -10,7 +10,6 @@ if (CModule::IncludeModule("iblock")) {
     if ($ar_res = $res->GetNext()) {
         // Получаем основные параметры товара
         $productName = $ar_res['NAME'];
-        $productShortName = CFile::GetPath($ar_res['EL_SHORTNAME']);
         $productDescription = $ar_res['DETAIL_TEXT'];
         $productImage = CFile::GetPath($ar_res['EL_IMAGES']); // Изображение товара
         $productPrice = ''; // Цена
@@ -54,7 +53,7 @@ if (CModule::IncludeModule("iblock")) {
 ?>
 
 <div class="section-title">
-    <h2><?php echo htmlspecialchars($productShortName); ?></h2> <!-- Название товара -->
+    <h2><?php echo htmlspecialchars($productName); ?></h2> <!-- Название товара -->
     <p>Подробное описание</p>
 </div>
 
