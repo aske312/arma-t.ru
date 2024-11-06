50e5272f6e95d347f5edbdae27884b85198b26e0
Aske312 <Aske312@yandex.ru>
2024-11-06 05:00:55 +0300

    fix

--- public_html/bitrix/templates/home/header.php
+++ public_html/bitrix/templates/home/header.php
@@ -39,7 +39,7 @@ $cartItems = isset($_SESSION['cartItems']['cartItems']) ? $_SESSION['cartItems']
                 <button class="menu-toggle" onclick="toggleMenu()">&#9776;</button>
                 <nav id="mainNav">
                     <a href="/">О компании</a>
-                    <a href="/catalog/index.php?SECTION_ID=1">Каталог</a>
+                    <a href="/catalog/index.php?SECTION_ID=22">Каталог</a>
                     <a href="/#contact">Контакты</a>
                     <a href="/#delivery">Доставка</a>
                     <a href="/#pay">Оплата</a>
