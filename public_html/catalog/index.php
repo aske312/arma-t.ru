b626890b9ab833fd5410cadf1c54b507ca1f6a04
Aske312 <Aske312@yandex.ru>
2024-11-07 21:21:33 +0300

    fix

--- public_html/catalog/index.php
+++ public_html/catalog/index.php
@@ -144,15 +144,17 @@ echo '</pre>';
         <!-- Фильтры -->
         <div class="catalog-filters">
             <?php foreach ($filterProperties as $propertyCode): ?>
-                <div class="filter">
-                    <label for="<?= $propertyCode ?>"><?= GetMessage($propertyCode); ?></label>
-                    <select id="<?= $propertyCode ?>" name="<?= $propertyCode ?>" onchange="applyFilter()">
-                        <option value="all">Все</option>
-                        <?php foreach ($filterValues[$propertyCode] as $value): ?>
-                            <option value="<?= $value ?>" <?= ($_GET[$propertyCode] == $value) ? 'selected' : ''; ?>><?= $value ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
+                <?php if (!empty($filterValues[$propertyCode])): ?>
+                    <div class="filter">
+                        <label for="<?= $propertyCode ?>"><?= GetMessage($propertyCode); ?></label>
+                        <select id="<?= $propertyCode ?>" name="<?= $propertyCode ?>" onchange="applyFilter()">
+                            <option value="all">Все</option>
+                            <?php foreach ($filterValues[$propertyCode] as $value): ?>
+                                <option value="<?= $value ?>" <?= ($_GET[$propertyCode] == $value) ? 'selected' : ''; ?>><?= $value ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                <?php endif; ?>
             <?php endforeach; ?>
         </div>
 
