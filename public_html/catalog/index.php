8f74bbe64404f1a342945dbb182836d07d8876a2
Aske312 <Aske312@yandex.ru>
2024-11-07 22:29:13 +0300

    fix

--- public_html/catalog/index.php
+++ public_html/catalog/index.php
@@ -154,17 +154,15 @@ $arResult['NAV_STRING'] = $res->GetPageNavStringEx($navComponentObject, "", ".de
         <!-- Фильтры -->
         <div class="catalog-filters">
             <?php foreach ($filterProperties as $propertyCode): ?>
-                <?php if (!empty($filterValues[$propertyCode])): ?>
-                    <div class="filter">
-                        <label for="<?= $propertyCode ?>"><?= GetMessage($propertyCode); ?></label>
-                        <select id="<?= $propertyCode ?>" name="<?= $propertyCode ?>" onchange="applyFilter()">
-                            <option value="all">Все</option>
-                            <?php foreach ($filterValues[$propertyCode] as $value): ?>
-                                <option value="<?= $value ?>" <?= ($_GET[$propertyCode] == $value) ? 'selected' : ''; ?>><?= $value ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                <?php endif; ?>
+                <div class="filter">
+                    <label for="<?= $propertyCode ?>"><?= GetMessage($propertyCode); ?></label>
+                    <select id="<?= $propertyCode ?>" name="<?= $propertyCode ?>" onchange="applyFilter()">
+                        <option value="all">Все</option>
+                        <?php foreach ($filterValues[$propertyCode] as $value): ?>
+                            <option value="<?= $value ?>" <?= ($_GET[$propertyCode] == $value) ? 'selected' : ''; ?>><?= $value ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
             <?php endforeach; ?>
         </div>
 
