f1a16d1f0f54a110b771085c16d9039c7052c3d1
Aske312 <Aske312@yandex.ru>
2024-11-10 20:48:55 +0300

    fix

--- public_html/catalog/index.php
+++ public_html/catalog/index.php
@@ -173,9 +173,9 @@ $arResult['NAV_STRING'] = $res->GetPageNavStringEx($navComponentObject, "", ".de
                 <div class="filter">
                     <?php if ($propertyCode == 'EL_DN_DIAMETER_MM'): ?>
                         <div class="filter-item">
-                            <label class="filter-label">Диаметр DN:</label>
+                            <label class="filter-label"></label>
                             <div class="dropdown">
-                                <button class="dropdown-toggle" onclick="toggleDropdown('filterDN')">Выберите значения</button>
+                                <button class="dropdown-toggle" onclick="toggleDropdown('filterDN')">Диаметр DN:</button>
                                 <div id="filterDN" class="dropdown-content">
                                     <div class="dropdown-options">
                                         <?php foreach ($values as $value): ?>
