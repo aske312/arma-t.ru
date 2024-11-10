90d2c5a8c0724b3564331bb9cf5b4f230e01167b
Aske312 <Aske312@yandex.ru>
2024-11-11 00:28:51 +0300

    fix

--- public_html/catalog/index.php
+++ public_html/catalog/index.php
@@ -203,6 +203,7 @@ $arResult['NAV_STRING'] = $res->GetPageNavStringEx($navComponentObject, "", ".de
                                     <?php endforeach; ?>
                                 </div>
                                 <button class="apply-button" id="applyFilterButton-<?= $propertyCode ?>" onclick="applyFilter('<?= $propertyCode ?>')" style="display: none;">Показать</button>
+                                <button id="applyFiltersButton" style="display: none;" onclick="applyFilter()">Показать</button>
                             </div>
                         </div>
                     </div>
