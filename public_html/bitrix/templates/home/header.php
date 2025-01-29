e1a3a35a98aaa92b3d159ad9ec79abab3eedd50a
Aske312 <Aske312@yandex.ru>
2025-01-30 00:54:04 +0300

    fix

--- public_html/bitrix/templates/home/header.php
+++ public_html/bitrix/templates/home/header.php
@@ -249,7 +249,7 @@ if ($element = $res->Fetch()) {
                             <input type="number" class="quantity-input" value="${item.quantity}" onchange="updateQuantityManual('${item.id}', this.value)">
                             <button class="quantity-btn" onclick="updateQuantity('${item.id}', 1)">&#43;</button>
                             <span class="cart-item-total">
-                              ${!isNaN(itemTotal) && itemTotal !== null ? itemTotal + ' руб.' : 'Через оформление'}
+                              ${!isNaN(itemTotal) && itemTotal !== null ? itemTotal + ' руб.' : ''}
                             </span>
                             <span class="remove-item-btn" onclick="removeCartItem('${item.id}')">&#10005;</span>
                         </div>
