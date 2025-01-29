7c49fe87ce4b5a5d1e6638ef7cbca8995875c404
aske.312 <aske312@yandex.ru>
2025-01-29 17:41:07 +0300

    fix by notebook

--- public_html/catalog/new_index.php
+++ public_html/catalog/new_index.php
@@ -38,20 +38,6 @@
     $queryParams = array_merge(['SECTION_ID' => $sectionId], $_GET);
     $queryString = http_build_query($queryParams, '', '&');
 
-//     // Выводим отфильтрованный каталог
-//     $APPLICATION->IncludeComponent(
-//         "bitrix:catalog.section",
-//         "",
-//         [
-//             "FILTER_NAME" => "sectionFilter",
-//             "IBLOCK_ID" => 1,
-//             "SECTION_ID" => $sectionId,
-//             "INCLUDE_SUBSECTIONS" => "Y",
-//             "FILTER_VALUES" => $filters,
-//         ],
-//         false
-//     );
-
     // Получаем список всех активных секций для бокового меню
     $sectionsFilter = [
         'IBLOCK_ID' => 1,
