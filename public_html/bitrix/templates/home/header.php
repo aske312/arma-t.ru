716a2459956eae3a10543f4d86071f526c6fdfff
Aske312 <Aske312@yandex.ru>
2025-01-30 01:53:04 +0300

    fix

--- public_html/bitrix/templates/home/header.php
+++ public_html/bitrix/templates/home/header.php
@@ -322,12 +322,6 @@ if ($element = $res->Fetch()) {
             window.location.href = '/checkout/';
         });
 
-//        document.getElementById('cart-button').addEventListener('click', function() {
-//            const cartModal = document.getElementById('cart-modal');
-//            cartModal.style.display = cartModal.style.display === 'block' ? 'none' : 'block';
-//            loadCartData();
-//        });
-
         document.getElementById('close-cart-modal').addEventListener('click', function() {
             document.getElementById('cart-modal').style.display = 'none';
         });
