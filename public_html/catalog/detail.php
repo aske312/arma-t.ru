dacf629b36ccd8e54c028f1008f0c6627638af99
Aske312 <Aske312@yandex.ru>
2025-01-30 01:07:20 +0300

    fix

--- public_html/catalog/detail.php
+++ public_html/catalog/detail.php
@@ -85,7 +85,7 @@ if (CModule::IncludeModule("iblock")) {
                 <p>Цена: <?php echo htmlspecialchars($productPrice); ?></p>
             </div>
             <button class="add-to-cart" data-id="<?php echo $productId; ?>"
-                    data-name="<?php echo htmlspecialchars($productName); ?>"
+                    data-name="<?php echo htmlspecialchars($productShortName); ?>"
                     data-price="<?php echo htmlspecialchars($productPrice); ?>"
                     data-articul="<?php echo htmlspecialchars($productArticul); ?>"
                     onclick="addToCart(this)">
