e2603c646be8a219f094623f56e903abcbf9aa16
Aske312 <Aske312@yandex.ru>
2025-01-30 07:50:08 +0300

    fix

--- public_html/bitrix/templates/home/description.php
+++ public_html/bitrix/templates/home/description.php
@@ -3,7 +3,7 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 	die();
 
 $arTemplate = Array(
-	'NAME' => 'Home',
-	'DESCRIPTION' => 'Home '
+	'NAME' => 'ARMA-T',
+	'DESCRIPTION' => 'Надежный поставщик для вашего бизнеса'
 );
 ?>
