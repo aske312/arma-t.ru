90112423d1f59abe542fea25aea22f454f919281
Aske312 <Aske312@yandex.ru>
2025-01-30 06:51:58 +0300

    fix

--- public_html/bitrix/templates/home/header.php
+++ public_html/bitrix/templates/home/header.php
@@ -377,22 +377,6 @@ if ($element = $res->Fetch()) {
             xhr.send(formData);
         });
 
-        if (xhr.status === 429) {
-            const response = JSON.parse(xhr.responseText);
-            const remainingTime = parseInt(response.message.match(/\d+/)[0]); // Извлекаем оставшееся время
-            alert(`Пожалуйста, подождите ${remainingTime} секунд перед повторной отправкой.`);
-
-            // Запускаем таймер
-            let timer = remainingTime;
-            const timerInterval = setInterval(() => {
-                timer--;
-                if (timer <= 0) {
-                    clearInterval(timerInterval);
-                    alert('Теперь вы можете отправить форму снова.');
-                }
-            }, 1000);
-        }
-
         // Функция для изменения размеров окна корзины
         function setupCartModalResize() {
             const cartModalContent = document.querySelector('.cart-modal-content');
