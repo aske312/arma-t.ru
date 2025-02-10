5bd0e2335331935b4c5e3b42514eb286c9c97101
aske.312 <aske312@yandex.ru>
2025-02-10 17:38:10 +0300

    add metrika detail page

--- public_html/catalog/detail.php
+++ public_html/catalog/detail.php
@@ -58,6 +58,24 @@ if (CModule::IncludeModule("iblock")) {
         }
 ?>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99863715, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/99863715" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <div class="section-title">
     <h2>Подробное описание</h2>
     <p><?php echo htmlspecialchars_decode($productShortName); ?></p>
