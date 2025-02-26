3ffceb967b1416ec4a61ba52fa648480399d5298
aske.312 <aske312@yandex.ru>
2025-02-26 14:30:34 +0300

    header flex

--- public_html/index_mobile.php
+++ public_html/index_mobile.php
@@ -10,7 +10,7 @@ use Bitrix\Iblock;
 Loader::includeModule('iblock');
 
 Asset::getInstance()->addCss("/resources/css/home.css"); // CSS
-Asset::getInstance()->addJs("/resources/js/script.js"); // JS
+// Asset::getInstance()->addJs("/resources/js/script.js"); // JS
 
 // Получение данных для слайдера (ID = 1)
 $sliderItems = [];
@@ -95,7 +95,6 @@ while ($section = $sections->Fetch()) {
 <noscript><div><img src="https://mc.yandex.ru/watch/99863521" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
-
 <!-- -->
 <div class="section section1">
     <div class="slider-wrapper">
@@ -119,204 +118,7 @@ while ($section = $sections->Fetch()) {
     </div>
 </div>
 
-<!--
-<div class="section section2" id="Company">
-	<div class="container">
-		<div class="content">
-			<div class="image">
-                <img alt="Image" src="/resources/img/block/312asd.png">
-			</div>
-			<div class="text">
-				<p>АРМА-Т - специализируется на продаже запорной арматуры. Мы предлагаем широкий ассортимент продукции.
-				У нас вы найдете все необходимое для обеспечения надежной работы трубопроводных систем.
-				Обращайтесь к нам, ведь мы гарантируем быструю доставку товаров высокого качества и отличный сервис!</p>
-			</div>
-		</div>
-	</div>
-</div>
--->
-
-<!--
-<div class="section section3" id="Delivery">
-	<div class="block">
-		<div class="block-item">
-			<div class="block-image">
-                <img alt="Image 1" src="/resources/img/block/res1.png">
-			</div>
-			<div class="item-text">
-				 Наши специалисты помогут <br>
-				 с выбором продукции
-			</div>
-		</div>
-		<div class="block-item">
-			<div class="block-image">
-                <img alt="Image 2" src="/resources/img/block/res2.png">
-			</div>
-			<div class="item-text">
-				 Выставим счет <br>
-				 в течение 2-3 часов
-			</div>
-		</div>
-		<div class="block-item">
-			<div class="block-image">
-                <img alt="Image 3" src="/resources/img/block/res3.png">
-			</div>
-			<div class="item-text">
-				 Осуществим доставку <br>
-				 транспортной компанией <br>
-				 по вашему выбору
-			</div>
-		</div>
-	</div>
-</div>
--->
-
-<!--
-<div class="section section4-title" id="catalog">
-    <div class="category-title">
-        <h2>Каталог</h2>
-    </div>
-</div>
-
-<div class="section section4" id="catalog">
-    <div class="category">
-        <div class="category-table">
-            <?php if (!empty($arResult['SECTIONS'])): ?>
-                <?php foreach ($arResult['SECTIONS'] as $arSection): ?>
-                    <div class="category-block" onclick="redirectToSection(<?= $arSection['ID']; ?>)">
-                        <?php if ($arSection['PICTURE']): ?>
-                            <?php $imgPath = CFile::GetPath($arSection['PICTURE']); ?>
-                            <img alt="<?= $arSection['NAME']; ?>" src="<?= $imgPath; ?>">
-                        <?php else: ?>
-                            <img alt="Нет изображения" src="/resources/img/no_image.png">
-                        <?php endif; ?>
-                        <div class="category-text"><?= $arSection['NAME']; ?></div>
-                    </div>
-                <?php endforeach; ?>
-            <?php else: ?>
-                Нет доступных категорий
-            <?php endif; ?>
-        </div>
-    </div>
-</div>
--->
-
-<!--
-<div class="section section6-title" id="delivery-title">
-    <div class="delivery-title">
-        <h2>Доставка</h2>
-    </div>
-</div>
-
-<div class="section section6" id="delivery">
-    <div class="d-container">
-        <div class="d-content">
-            <div class="d-text">
-                <div class="d-image">
-                    <img alt="Image" src="/resources/img/block/312asd.png">
-                </div>
-                <div class="d-text-content">
-                    <p> Доступен самовывоз продукции со склада.
-                    В случае самовывоза потребуется печать или доверенность от организации.
-                    Осуществим доставку транспортной компанией по вашему выбору.
-                    При необходимости доставки вы можете уведомить об этом менеджера перед оформлением заказа
-                    (сообщите адрес и способ доставки).  </p>
-                </div>
-            </div>
-            <div class="d-list">
-                <div class="d-icons">
-                    <div class="d-icon">
-                        <img src="/resources/img/companies/dellin.png" alt="Деловые Линии">
-                    </div>
-                    <div class="d-icon">
-                        <img src="/resources/img/companies/pek.jpg" alt="ПЭК">
-                    </div>
-                    <div class="d-icon">
-                        <img src="/resources/img/companies/baikal.jpg" alt="Байкал Сервис">
-                    </div>
-                    <div class="d-icon">
-                        <img src="/resources/img/companies/sdek.png" alt="СДЭК">
-                    </div>
-                </div>
-            </div>
-            <div class="d-title-list">
-                <p>Мы работаем с известными транспортными компаниями.</p>
-            </div>
-        </div>
-    </div>
-</div>
--->
-
-<!--
-
-<div class="section section5" id="Cash">
-    <div class="form-container" id="Form">
-        <form class="contact-form" id="contactForm" method="post" enctype="multipart/form-data">
-            <h2>Оставить заявку</h2>
-            <input type="text" id="name" name="name" placeholder="Ваше Имя" required>
-            <input type="email" id="email" name="email" placeholder="e-mail" required>
-            <input type="text" id="subject" name="subject" placeholder="Название компании" required>
-            <textarea id="message" name="message" rows="5" placeholder="Комментарий"></textarea>
-
-            <div class="form-actions">
-                <button type="submit" id="submitButton">Отправить</button>
-                <label for="newsletter" class="newsletter-label">
-                    <input type="checkbox" id="newsletter" name="newsletter" required>
-                    Согласие на рассылку
-                </label>
-            </div>
-        </form>
-    </div>
-</div>
-
--->
-
-<!--
-
-<div class="section section7-title" id="pay-title">
-    <div class="pay-title">
-        <h2>Оплата</h2>
-    </div>
-</div>
-
-<div class="section section7" id="pay">
-	<div class="p-container">
-	     <div class="p-content">
-            <div class="p-text">
-                <div class="p-image">
-                    <img alt="Image" src="/resources/img/block/312asd.png">
-                </div>
-                <div class="p-text-content">
-                    <p>Вы можете оплатить заказ по безналичному расчету, через любой банк РФ. Для выставления счета от
-                    вас потребуется - карточка организации с банковскими реквизитами, телефон, адрес доставки,
-                    а также электронный адрес. Деньги поступают на расчетный счет на следующий день после оплаты.
-                    Статус поступления средств можно уточнить у нашего менеджера.</p>
-                </div>
-            </div>
-        </div>
-	</div>
-</div>
-
--->
-
-
 <script>
-    function toggleMenu() {
-        var nav = document.getElementById('mainNav');
-        nav.classList.toggle('menu-open');
-    }
-    window.onscroll = function() {stickyHeader()};
-
-    var header = document.getElementById("siteHeader");
-    var sticky = header.offsetTop;
-
-    function stickyHeader() {
-        if (window.pageYOffset > sticky) {
-            header.classList.add("fixed");
-        } else {
-            header.classList.remove("fixed");
-        }
-    }
 
     function redirectToSection(sectionId) {
         // Строим URL для страницы каталога, передавая параметр SECTION_ID
