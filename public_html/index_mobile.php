236c49d54e27e5f5c805dcb04c52439466e81f5f
aske.312 <aske312@yandex.ru>
2025-02-26 16:34:27 +0300

    header flex

--- public_html/index_mobile.php
+++ public_html/index_mobile.php
@@ -121,9 +121,11 @@ while ($section = $sections->Fetch()) {
 <div class="section-2-m">
     <div class="container-m">
         <div class="content-m">
+            <!--
             <div class="image-m">
                 <img alt="Image" src="/resources/img/block/312asd.png">
             </div>
+            -->
             <div class="text-m">
                 <p>АРМА-Т - специализируется на продаже запорной арматуры. Мы предлагаем широкий ассортимент продукции.
                 У нас вы найдете все необходимое для обеспечения надежной работы трубопроводных систем.
