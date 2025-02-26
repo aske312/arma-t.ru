1163e5f633151098d7b4c1abf5b4037d7e42356b
aske.312 <aske312@yandex.ru>
2025-02-26 10:35:08 +0300

    header flex

--- public_html/bitrix/templates/home/header-mobile.php
+++ public_html/bitrix/templates/home/header-mobile.php
@@ -107,9 +107,7 @@ if ($element = $res->Fetch()) {
     <script>
         function toggleMenu() {
             const nav = document.getElementById('mobileMenu');
-            const header = document.getElementById('siteHeader');
-            nav.classList.toggle('menu-open');
-            header.classList.toggle('menu-expanded');
+            nav.classList.toggle('open');
         }
 
         window.addEventListener('scroll', function() {
