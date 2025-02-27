c8fdada4bbb99bf9d9dc3f27ed47ca39c743f038
aske.312 <aske312@yandex.ru>
2025-02-27 12:25:30 +0300

    footer_mobile fix

--- public_html/bitrix/templates/home/footer_mobile.php
+++ public_html/bitrix/templates/home/footer_mobile.php
@@ -51,7 +51,7 @@
         <!-- Блок с картой -->
         <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
         <div class="map-m">
-            <div id="map" style="width: 475px; height: 475px;"></div>
+            <div id="map" style="width: 350px; height: 350px;"></div>
             <script>
                 ymaps.ready(init);
                 function init() {
