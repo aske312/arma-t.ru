fed06b83ca5c7127cee88b88824b9b85c9c7b290
Aske312 <Aske312@yandex.ru>
2025-03-03 11:03:09 +0300

    refactor

--- public_html/bitrix/templates/home/description.php
+++
@@ -1,9 +0,0 @@
-<?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
-	die();
-
-$arTemplate = Array(
-	'NAME' => 'ARMA-T',
-	'DESCRIPTION' => 'Надежный поставщик для вашего бизнеса'
-);
-?>
