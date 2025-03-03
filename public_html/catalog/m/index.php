fff9854b957145e07d48c94d06d4c76194f36a11
Aske312 <Aske312@yandex.ru>
2025-03-03 10:57:46 +0300

    refactor

--- public_html/catalog/m/index.php
+++ public_html/catalog/m/index.php
@@ -248,7 +248,7 @@ $arResult['NAV_STRING'] = $res->GetPageNavStringEx($navComponentObject, "", ".de
             ?>
 
             <div class="catalog-item-m" data-id="<?= $arFields['ID']; ?>">
-                <a href="detail.php?ID=<?= $arFields['ID']; ?>" class="catalog-item-link-m">
+                <a href="detail/index.php?ID=<?= $arFields['ID']; ?>" class="catalog-item-link-m">
                     <div class="catalog-item-header-m">
                         <img src="<?= $productImage; ?>" alt="<?= $arFields['NAME']; ?>" class="catalog-item-image-m">
                     </div>
@@ -308,7 +308,7 @@ $arResult['NAV_STRING'] = $res->GetPageNavStringEx($navComponentObject, "", ".de
         item.addEventListener('click', (event) => {
             // Если клик произошел не на кнопке "В корзину", переходим на детальную страницу
             if (!event.target.closest('.catalog-item-add-to-cart-m')) {
-                window.location.href = `/catalog/detail.php?ID=${item.getAttribute('data-id')}`;
+                window.location.href = `/detail/index.php?ID=${item.getAttribute('data-id')}`;
             }
         });
     });
