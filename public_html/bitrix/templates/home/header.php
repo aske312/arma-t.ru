65b012c5feb0114555d8ccfed1480304a6dfed16
aske.312 <aske312@yandex.ru>
2025-03-17 16:11:25 +0300

    fix

--- public_html/bitrix/templates/home/header.php
+++ public_html/bitrix/templates/home/header.php
@@ -92,7 +92,7 @@ if ($element = $res->Fetch()) {
                 <!-- <button class="menu-toggle" onclick="toggleMenu()">&#9776;</button> -->
                 <nav id="mainNav">
                     <a href="/">О компании</a>
-                    <a href="/catalog/index.php?SECTION_ID=1">Каталог</a>
+                    <a href="/catalog/index.php?SECTION_ID=2">Каталог</a>
                     <a href="/#contact">Контакты</a>
                     <a href="/#delivery-title">Доставка</a>
                     <a href="/#pay">Оплата</a>
