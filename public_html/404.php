030924eb396fe3234e54a921ef9e8b1bb7fc5205
aske.312 <aske312@yandex.ru>
2025-03-21 11:26:01 +0300

    fix from detail

--- public_html/404.php
+++ public_html/404.php
@@ -4,7 +4,7 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.
 CHTTP::SetStatus("404 Not Found");
 @define("ERROR_404","Y");
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+// require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("404 Not Found");
 
