25597e99769d6af1982189027b61956728ea5a6a
aske.312 <aske312@yandex.ru>
2025-03-23 23:11:14 +0300

    fix map

--- public_html/bitrix/templates/home/footer.php
+++ public_html/bitrix/templates/home/footer.php
@@ -60,11 +60,11 @@ if ($isMobile) {
         </div>
 
         <!-- Блок с картой -->
-        <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+        <!-- <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script> -->
         <div class="map">
             <iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=168551027459" width="475" height="475" frameborder="1"></iframe>
             <!-- <div id="map" style="width: 475px; height: 475px;"></div> -->
-            <script>
+            <!-- <script>
 //                 ymaps.ready(init);
 //                 function init() {
 //                     var myMap = new ymaps.Map("map", {
@@ -79,7 +79,7 @@ if ($isMobile) {
 //
 //                     myMap.geoObjects.add(myPlacemark);
 //                 }
-            </script>
+            </script>-->
         </div>
     </div>
 </div>
